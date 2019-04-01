967d43e2e4e472334b2379767234b2b08f8ffe9c
Hamed <info.hamedjafari@gmail.com>
2019-04-01 15:58:48 +0200

    Updates

---
+++ precache-manifest.6ee7d0a5e7391f1dc4aa359263aeaf49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58120db414c74861c782",
+    "url": "/static/js/main.adcca952.chunk.js"
+  },
+  {
+    "revision": "fea31fda4f073c83d522",
+    "url": "/static/js/2.1ca0ea5d.chunk.js"
+  },
+  {
+    "revision": "58120db414c74861c782",
+    "url": "/static/css/main.73f39f65.chunk.css"
+  },
+  {
+    "revision": "fea31fda4f073c83d522",
+    "url": "/static/css/2.5e50a9d7.chunk.css"
+  },
+  {
+    "revision": "d3ca85331d1aed9823d226c139f24401",
+    "url": "/index.html"
+  }
+];
